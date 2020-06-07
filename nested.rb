7bd95e7495fec6a3a7803ebd6689317bdc39733b
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-07 19:00:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] => { :known_for => "Ruby",
+  :languages => ["LISP", "C"]} 
+  programmer_hash
 end
 
 #binding.pry
