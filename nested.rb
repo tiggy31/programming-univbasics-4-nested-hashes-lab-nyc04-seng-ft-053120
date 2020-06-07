539ed1509cc415d719c798e82f7e1c4c3713c92e
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-07 19:02:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,7 +60,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-    programmer_hash[:dennis_ritchie] [:known_for]
+    programmer_hash[:dennis_ritchie] [:languages]
 end
 
 
