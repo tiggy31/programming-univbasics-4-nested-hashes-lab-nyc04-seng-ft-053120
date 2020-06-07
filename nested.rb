2b4fc5d20ac11a0d63a7dd1708763a19ebff1eb3
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-07 18:54:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,6 +107,7 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    alans_new_info = "RUBY"
     	programmer_hash[:alan_kay][:known_for] = alans_new_info
     	 programmer_hash
 end
