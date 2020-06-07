39e16cca54ac4108ba82eb3ea3d3a4888c309820
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-07 19:03:09 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,11 +1,11 @@
-
+                  require "pry"
 def hopper
-	programmer_hash = 
- 		{
-      :grace_hopper => {
+
+	programmer_hash = {:grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
+  
       :alan_kay => {
         :known_for => "Object Orientation",
         :languages => ["Smalltalk", "LISP"]
@@ -15,8 +15,12 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper]
+    
 end
+
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -36,8 +40,9 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -52,11 +57,12 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+    programmer_hash[:dennis_ritchie] [:languages]
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -81,8 +87,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto] ={:known_for => "Ruby",
+  :languages => ["LISP", "C"]} 
+  programmer_hash
 end
+
+#binding.pry
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,7 +113,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    alans_new_info = "GUI"
+    	programmer_hash[:alan_kay][:known_for] = alans_new_info
+    	 programmer_hash
 end
 
 def adding_to_dennis
@@ -124,6 +136,8 @@ def adding_to_dennis
         :known_for => "Unix",
         :languages => ["C"]
       }
+     
     }
-
+ programmer_hash[:dennis_ritchie ][:languages] << "Assembly" 
+ 	programmer_hash 
 end
